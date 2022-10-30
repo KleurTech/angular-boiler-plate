767c2fbc7e06eb40b528ebae641069a2ba6bf800
startupshive <shubh.startupshive@gmail.com>
2022-10-31 03:04:35 +0530

    init setup

---
+++ ang-bp/src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  deployUrl: 'http://localhost:3000/api/',
+  // deployedUrl: 'http://localhost:3000/api/',
+  deployedUrl: "https://irm-be.azurewebsites.net/api/",
+  // deployedUrl: 'https://localhost:7216/api/',
+  // deployedUrl: 'https://pitchdeck20221001000128.azurewebsites.net/api/',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+ 
